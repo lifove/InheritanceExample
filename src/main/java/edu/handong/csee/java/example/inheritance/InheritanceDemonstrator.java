6ec37703d71fa92647bbab43ea01c4729bf53ce9
JC <jc@lifove.net>
2018-04-15 18:23:18 +0900

    Add inheritance Demonstrator

---
+++ src/main/java/edu/handong/csee/java/example/inheritance/InheritanceDemonstrator.java
@@ -0,0 +1,12 @@
+package edu.handong.csee.java.example.inheritance;
+
+public class InheritanceDemonstrator {
+
+	public static void main(String[] args) {
+		 Student s = new Student();
+		 s.setName("Warren Peace");
+		 s.setStudentNumber(1234);
+		 s.writeOutput();
+	}
+
+}
